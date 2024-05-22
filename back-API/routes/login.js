c1367d2d31c5ac65123ede8badb4e5dd3b5a2686
mike_medina <maicolmedinafernandez@gmail.com>
2024-05-22 12:32:27 -0500

    login

--- back-API/routes/login.js
+++ back-API/routes/login.js
@@ -1,5 +1,26 @@
+const { jsonResponse } = require("../lib/jsonResponse");
 const router = require("express").Router();
-router.get("/",(req,res)=>{
-    res.send("login");
+
+router.post("/",(req,res)=>{
+    const{username, password} = req.body;
+
+    if(!!!username || !!!password ){
+        return res.status(400).json(jsonResponse(400,{
+            error: "Campos Requeridos",
+        })
+    );
+    }
+
+    //autenticacion de usuario
+    const accessToken = "access:token";
+    const refreshToken = "refresh_token";
+    const user = {
+        id:'1',
+        name: 'Michael Medina',
+        username: 'xxxxxxx'
+    };
+    res.status(200).json(jsonResponse(200,{user, accessToken, refreshToken}));
+    
+    res.send("signout");
 });
 module.exports = router;
