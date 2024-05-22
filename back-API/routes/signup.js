1da486d1aae151794f1a810b626d7d1608bfcc75
mike_medina <maicolmedinafernandez@gmail.com>
2024-05-22 11:45:14 -0500

    cambios

--- back-API/routes/signup.js
+++ back-API/routes/signup.js
@@ -1,5 +1,18 @@
+const { jsonResponse } = require("../lib/jsonResponse");
 const router = require("express").Router();
-router.get("/",(req,res)=>{
-    res.send("signup");
+router.post("/",(req,res)=>{
+    const{name, email, username, password} = req.body;
+
+    if(!!name || !!email || !!username || !!password ){
+        return res.status(400).json(jsonResponse(400,{
+            error: "Campos Requeridos",
+        })
+    );
+    }
+
+    //crear el usuario
+    res.status(200).json(jsonResponse(200,{message: "Usuario creado Exitosamente"}));
+    
+    res.send("signout");
 });
 module.exports = router;
